43dd64b2d0a45e4c418243824fa32326addd1c6d
SorayaSantos <soraya.ss_3@hotmail.com>
2018-11-14 19:02:20 +0000

    aula 14 de novembro

--- StockManagement/src/io/altar/jseproject/model/Shelf.java
+++ StockManagement/src/io/altar/jseproject/model/Shelf.java
@@ -2,4 +2,50 @@ package io.altar.jseproject.model;
 
 public class Shelf {
 
+	private int id;
+	private int capacity;
+	private Product product;
+	private int price;
+	
+	public Shelf(int id, int capacity, Product product, int price) {
+		super();
+		this.id = id;
+		this.capacity = capacity;
+		this.product = product;
+		this.price = price;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	
 }
